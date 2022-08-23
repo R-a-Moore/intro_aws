a402f199c82bbf4f72a3c4800f5e06c0a79f149f
Christian <Moore.Christian76@gmail.com>
2022-08-23 10:37:44 +0100

    file management

--- provisions/provision.sh
+++ provisions/provision.sh
@@ -10,8 +10,9 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
   
 sudo apt-get install -y nodejs npm
 # stores an environment variable 
-# echo "DB_HOST=mongodb://192.168.10.150:27017/posts" | sudo tee -a /etc/environment
+echo "DB_HOST=mongodb://192.168.10.150:27017/posts" | sudo tee -a /etc/environment
 # uncomment this line once the db is available and replace the this ip with the instance db ip
+
 cd app
 
 cd app
@@ -23,4 +24,14 @@ sudo systemctl restart nginx
 sudo apt-get update -y
   
 sudo apt-get upgrade -y
+
+cd seeds
+
+node seed.js
+
+cd ..
+
+sudo npm install express
+
+npm start
 
